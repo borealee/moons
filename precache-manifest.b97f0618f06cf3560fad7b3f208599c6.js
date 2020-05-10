34b18093a411d80d113260a1118f7b70c2183781
bb <>
2020-05-10 12:01:44 +0300

    Updates

---
+++ precache-manifest.b97f0618f06cf3560fad7b3f208599c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163ab973c488f261cfc581ee49b1bdcb",
+    "url": "/moons/index.html"
+  },
+  {
+    "revision": "6e1f544e3cf610898696",
+    "url": "/moons/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "614f61cc9fc6da774be4",
+    "url": "/moons/static/js/2.464374a2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/moons/static/js/2.464374a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e1f544e3cf610898696",
+    "url": "/moons/static/js/main.7e6f8811.chunk.js"
+  },
+  {
+    "revision": "c71e4e0912d38f5a8ad4",
+    "url": "/moons/static/js/runtime-main.f4c2cb56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moons/static/media/logo.5d5d9eef.svg"
+  }
+]);
